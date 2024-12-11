48d768382a8577ddd2086f285de227ae310787bf
saifularafat <saifularafat.info@gmail.com>
2024-12-12 03:17:21 +0600

    winston npm us by back end server login and error handle

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const app = require("./app")
 const connectDataBse = require("./config/db")
+const logger = require("./controllers/loggerController")
 const { serverPort } = require("./secret")
 
 
 app.listen(serverPort, async () => {
-    console.log(`home tutor provider BD is running at http://localhost:${serverPort}`)
+    logger.log('info', `home tutor provider BD is running at http://localhost:${serverPort}`)
     await connectDataBse()
 })
