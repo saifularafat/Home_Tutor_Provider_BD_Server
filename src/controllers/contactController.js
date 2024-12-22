a95cb84499983f5332e3f811b0642c3dec521f3d
saifularafat <saifularafat.info@gmail.com>
2024-12-22 20:19:01 +0600

    contact all get and pagination

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,14 +1,36 @@
 const createError = require('http-errors');
 const { successResponse } = require("../Helper/responseController");
+const Contact = require('../models/contactModel');
+const { getContacts } = require('../services/contactService');
 
 const handelGetContacts = async (req, res, next) => {
     try {
+        const search = req.query.search || "";
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 12;
 
+        const searchRegExp = new RegExp(".*" + search + ".*", "i");
+        const filter = {
+            $or: [
+                { contactEmail: { $regex: searchRegExp } },
+                { contactName: { $regex: searchRegExp } },
+            ]
+        }
+        const contactData = await getContacts(page, limit, filter)
 
         return successResponse(res, {
             statusCode: 201,
-            message: `Return contact successfully.`,
-            payload: {}
+            message: `Return all contact successfully.`,
+            payload: {
+                contacts: contactData.contacts,
+                pagination: {
+                    totalPage: contactData.totalPage,
+                    currentPage: contactData.currentPage,
+                    previousPage: page - 1,
+                    nextPage: page + 1,
+                    totalNumberOfTuition: contactData.count
+                }
+            },
         })
     } catch (error) {
         next(error)
---
+++ src/services/contactService.js
@@ -0,0 +1,23 @@
+const createError = require('http-errors');
+const Contact = require('../models/contactModel');
+
+const getContacts = async (page = 1, limit = 12) => {
+    const contacts = await Contact.find()
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort({ createdAt: -1 });
+
+    if (!contacts) {
+        throw createError(404, 'Contact Not Found')
+    }
+    const count = await Contact.find().countDocuments();
+    return {
+        contacts,
+        count,
+        totalPage: Math.ceil(count / limit),
+        currentPage: page,
+    }
+}
+module.exports = {
+    getContacts,
+}
